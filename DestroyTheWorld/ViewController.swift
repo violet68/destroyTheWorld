e68d92049370069c7e120a5c75462c47281a3bf5
Fatemeh Balaei <fatemehbalaei@Fatemehs-MacBook-Air.local>
2018-10-30 18:13:02 +0330

    Initial Commit

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Fatemeh Balaei on 2018-10-30.
+//  Copyright © 2018 Fatemeh Balaei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
